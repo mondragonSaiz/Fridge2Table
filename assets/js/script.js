9e9abeaab4574ed6093d00c67fe921e2b9c1281b
David Mondragon <dragon18saiz@gmail.com>
2023-05-02 14:34:09 -0600

    Added message for invalid input, alt attribute for all images, fixed responsive bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,56 @@
-var cusineInput = document.querySelector("#cuisineSelect");
-var mealInput = document.querySelector("#mealTypeSelect");
-var receiptsInput = document.querySelector("#numberReceiptsSelect");
-var submitBoton = document.querySelector("#submit-btn");
+var ingredientsInput = document.querySelector("#inputIngredients");
+var numberInput = document.getElementById("number-recipes");
+var errorNotification = document.querySelector(".notification");
+//var cusineInput = document.querySelector("#cuisineSelect");
+//var mealInput = document.querySelector("#mealTypeSelect");
+//var receiptsInput = document.querySelector("#numberReceiptsSelect");
+//var submitBoton = document.querySelector("#submit-btn");
 var formEL = document.querySelector("form");
 var secu = 25;
 var mainRcontiner = document.querySelector("#main-recipe-container");
-const apiKEY = "1a0fb33a6fc54f18b3bcb4f7cf0eff6c";
+const apiKEY = "f08512879ef245b9b4b26071967fe10c";
+var formIngredientsLable = document.querySelector(".form-ing");
+// numberInput.addEventListener("change", function () {
+//   console.log("You selected: ", this.value);
+//   if (this.value === "3") {
+//     console.log("Eureka!!!");
+//   } else {
+//     console.log("what");
+//   }
+// });
+// ingredientsInput.addEventListener("change", (event) => {
+//   console.log("*", event.target.value);
+// });
+
+changeEvent;
+var changeEvent = function (event) {
+  console.log("***CAMBIO***");
+  console.log("***", event.target.value);
+};
+var ingredientsLabelHelp = document.querySelector("#ingredeientsHelp");
 
 var submitInfo = function (event) {
   event.preventDefault();
-  //var recipeCONT = document.querySelector("recipe-home");
-  //recipeCONT.remove();
-  console.log("Inside submit");
-  var ingredientsInput = document.querySelector("#inputIngredients");
+
   var ingredients = ingredientsInput.value.trim();
+  console.log("Inside submit");
+  console.log(ingredients);
   if (ingredients === "") {
     console.log("Invalid input");
+
+    ingredientsLabelHelp.classList.add("text-danger");
+    // var errorN = document.createElement("div");
+    // errorN.classList.add("notification", "is-danger");
+    // var errorBTN = document.createElement("button");
+    // errorBTN.classList.add("delete");
+    // errorN.append(errorBTN);
+    // errorN.append("Please enter a valid input");
+    // formIngredientsLable.append(errorN);
+
+    errorNotification.classList.add("notification-visible");
     return;
   }
-  var numberofrecipes = document.getElementById("number-recipes").value;
+  var numberofrecipes = numberInput.value;
   if (numberofrecipes === "----" || numberofrecipes === null) {
     numberofrecipes = 10;
   }
@@ -27,36 +59,11 @@ var submitInfo = function (event) {
   requestRecipe(ingredients, numberofrecipes);
 };
 
-// function dividirIngredientes(ingredients) {
-//   var ingredientsArray = ingredients.split(" ");
-//   if (ingredientsArray.length === 1) {
-//     ingredientsArray = ingredients;
-//   } else {
-//     for (var i = 0; i < ingredientsArray.length; i++) {
-//       if (i == ingredientsArray.length - 1) {
-//         ingredientsArray[i] += "";
-//       } else {
-//         ingredientsArray[i] + ",+";
-//       }
-//     }
-//   }
-//   console.log("ZZZ", ingredientsArray);
-//   requestRecipe(ingredientsArray);
-// }
-
 var requestRecipe = function (ingredientsArray, number) {
-  // var actualING = "";
-  // //console.log("INGREDIENTSSS", ingredientsArray);
-  // for (var i = 0; i < ingredientsArray.length; i++) {
-  //   //console.log("INGREDIENTZZZ", ingredientsArray[i]);
+  console.log("Inside Request Recipe");
+  console.log("INGGG", ingredientsArray);
+  console.log("NUMNUM", number);
 
-  //   if (i !== ingredientsArray.length - 1) {
-  //     actualING += ingredientsArray[i] + ",+";
-  //   } else {
-  //     actualING += ingredientsArray[i];
-  //   }
-  // }
-  //console.log("ACTUAL ING", actualING);
   var requestURL =
     "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" +
     apiKEY +
@@ -65,139 +72,33 @@ var requestRecipe = function (ingredientsArray, number) {
     "&number=" +
     number;
 
+  console.log("URL", requestURL);
+
   fetch(requestURL)
     .then((Response) => Response.json())
     .then((data) => renderRecipes(data, ingredientsArray));
 };
 
-var requestRandomRecipe = function () {
-  var randomRequestURL =
-    "https://api.spoonacular.com/recipes/random?number=10&apiKey=" + apiKEY;
-  fetch(randomRequestURL)
-    .then((Response) => Response.json())
-    .then((randomData) => renderOnLoad(randomData));
-};
-
-var renderOnLoad = function (randomData) {
-  console.log("RRR", randomData);
-  //console.log("Recipe title :", randomData.recipes[0].title);
-  console.log("LOCAL STORAGE", localStorage.getItem("hiddenC"));
-
-  var recipeContainer = document.createElement("div");
-  recipeContainer.classList.add("randomRecipe");
-  recipeContainer.style.maxWidth = "740px";
-
-  for (var i = 0; i < randomData.recipes.length; i++) {
-    var recipeName = randomData.recipes[i].title;
-    var recipeID = randomData.recipes[i].id;
-    var randomRecipeCard = document.createElement("div");
-    randomRecipeCard.classList.add(
-      "card",
-      "text-bg-dark",
-      "p-3",
-      "border-light-subtle",
-      "mb-3"
-    );
-    //assignRecipeTitleID(recipeID);
-    var randomRecipeImgageSrc = randomData.recipes[i].image;
-    //console.log("RECIPE TITLE", recipeName);
-    var ingredients = randomData.recipes[i].extendedIngredients;
-    //console.log("MISSED INGREDIENTS :", ingredients);
-
-    var rowDiv = document.createElement("div");
-    rowDiv.classList.add("row", "g-0");
-
-    var imgDiv = document.createElement("div");
-    imgDiv.classList.add("col-md-4");
-    var img = document.createElement("img");
-    img.setAttribute("src", randomRecipeImgageSrc);
-    img.classList.add("img-fluid", "rounded-start");
-    imgDiv.append(img);
-
-    var bodyContainer = document.createElement("div");
-    bodyContainer.classList.add("col-md-8");
-
-    var bodyDIV = document.createElement("div");
-    bodyDIV.classList.add("card-body");
-
-    var recipeTitle = document.createElement("h5");
-    recipeTitle.classList.add("card-title");
-    recipeTitle.textContent = recipeName;
-
-    var recipeDetails = document.createElement("a");
-    recipeDetails.setAttribute("data-set", recipeID);
-    recipeDetails.addEventListener("click", function (event) {
-      var parentEL = event.target.parentElement;
-      console.log("DATA SET", parentEL);
-      var parentDataSet = parentEL.getAttribute("data-set");
-      console.log("IDD", parentDataSet);
-      localStorage.setItem("recipeId", parentDataSet);
-      localStorage.setItem("recipeName", recipeName);
-    });
-    recipeDetails.classList.add("card-text", "c-link");
-    recipeDetails.style.textDecoration = "none";
-    recipeDetails.style.color = "white";
-    recipeDetails.setAttribute("href", "instructions.html");
-    recipeDetails.setAttribute("target", "_blank");
-    var detailsText = document.createElement("small");
-    detailsText.textContent = "See instruction details";
-    recipeDetails.append(detailsText);
-
-    var ingredientsText = document.createElement("p");
-    ingredientsText.classList.add("card-text");
-    ingredientsText.textContent = "Ingredients : ";
-
-    for (var j = 0; j < ingredients.length; j++) {
-      //console.log("INDIVIDUAL MI", ingredients[j].name);
-      ingredientsText.textContent += ingredients[j].name;
-      if (j !== ingredients.length - 1) {
-        ingredientsText.textContent += ", ";
-      } else {
-        ingredientsText.textContent += ".";
-      }
-    }
-
-    bodyDIV.append(recipeTitle);
-    bodyDIV.append(ingredientsText);
-    bodyDIV.append(recipeDetails);
-    bodyContainer.append(bodyDIV);
-
-    rowDiv.append(imgDiv);
-    rowDiv.append(bodyContainer);
-    randomRecipeCard.append(rowDiv);
-    recipeContainer.append(randomRecipeCard);
-    mainRcontiner.append(recipeContainer);
-  }
-  console.log("RECIPE CONTAINER", recipeContainer);
-};
-
-requestRandomRecipe();
-
 var renderRecipes = function (data, ingredients) {
-  //localStorage.setItem("hiddenC", "hidden");
-  //console.log("DDDTA", data);
   var recipeTrendContainer = document.querySelector(".randomRecipe");
-  //console.log("CONTAINER 2", recipeTrendContainer);
+  recipeTrendContainer.remove();
+
+  console.log("DDDTA", data);
+  console.log("In", ingredients);
+
   if (recipeTrendContainer === null) {
     return;
   }
-  recipeTrendContainer.remove();
-  console.log("Holi");
-  var child = recipeTrendContainer.lastElementChild;
-  console.log(child);
-  while (child) {
-    recipeTrendContainer.removeChild(child);
-    child = recipeTrendContainer.lastElementChild;
-  }
-  var trendingTitle = document.querySelector(".trending");
-  trendingTitle.innerHTML = "Displaying recipes with : " + ingredients;
+
+  var titleDesc = document.querySelector(".trending");
+  titleDesc.innerHTML = "Displaying recipes with : " + ingredients;
   console.log("RECIPE TITLE", data[0].title);
   var recipeIdArray = [];
   for (var i = 0; i < data.length; i++) {
     var recipeName = data[i].title;
     var recipeId = data[i].id;
     // recipeIdArray += recipeId;
-    // console.log("RECIPE ID", recipeId);
+    console.log("RECIPE NAME", recipeName);
     console.log("RECIPE ID Array", recipeIdArray);
     var recipeImgaeSrc = data[i].image;
     //console.log("RECIPE TITLE", recipeName);
@@ -222,6 +123,7 @@ var renderRecipes = function (data, ingredients) {
     imgDiv.classList.add("col-md-4");
     var img = document.createElement("img");
     img.setAttribute("src", recipeImgaeSrc);
+    img.setAttribute("alt", recipeName);
     img.classList.add("img-fluid", "rounded-start");
     imgDiv.append(img);
 
@@ -287,11 +189,125 @@ var renderRecipes = function (data, ingredients) {
   // };
 };
 
-formEL.addEventListener("submit", submitInfo);
-
-var assignRecipeTitleID = function (id) {
-  var recipeNameID;
-  return (recipeNameID = id);
+var requestRandomRecipe = function () {
+  var randomRequestURL =
+    "https://api.spoonacular.com/recipes/random?number=10&apiKey=" + apiKEY;
+  fetch(randomRequestURL)
+    .then((Response) => Response.json())
+    .then((randomData) => renderOnLoad(randomData));
 };
 
-export { secu };
+var renderOnLoad = function (randomData) {
+  console.log("RRR", randomData);
+  //console.log("Recipe title :", randomData.recipes[0].title);
+  console.log("LOCAL STORAGE", localStorage.getItem("hiddenC"));
+
+  var recipeContainer = document.createElement("div");
+  recipeContainer.classList.add("randomRecipe");
+  recipeContainer.style.maxWidth = "740px";
+
+  for (var i = 0; i < randomData.recipes.length; i++) {
+    var recipeName = randomData.recipes[i].title;
+    var recipeID = randomData.recipes[i].id;
+    var randomRecipeCard = document.createElement("div");
+    randomRecipeCard.classList.add(
+      "card",
+      "text-bg-dark",
+      "p-3",
+      "border-light-subtle",
+      "mb-3"
+    );
+    //assignRecipeTitleID(recipeID);
+    var randomRecipeImgageSrc = randomData.recipes[i].image;
+    //console.log("RECIPE TITLE", recipeName);
+    var ingredients = randomData.recipes[i].extendedIngredients;
+    //console.log("MISSED INGREDIENTS :", ingredients);
+
+    var rowDiv = document.createElement("div");
+    rowDiv.classList.add("row", "g-0");
+
+    var imgDiv = document.createElement("div");
+    imgDiv.classList.add("col-md-4");
+    var img = document.createElement("img");
+    img.setAttribute("src", randomRecipeImgageSrc);
+    img.setAttribute("alt", recipeName);
+    img.classList.add("img-fluid", "rounded-start");
+    imgDiv.append(img);
+
+    var bodyContainer = document.createElement("div");
+    bodyContainer.classList.add("col-md-8");
+
+    var bodyDIV = document.createElement("div");
+    bodyDIV.classList.add("card-body");
+
+    var recipeTitle = document.createElement("h5");
+    recipeTitle.classList.add("card-title");
+    recipeTitle.textContent = recipeName;
+
+    var recipeDetails = document.createElement("a");
+    recipeDetails.setAttribute("data-set", recipeID);
+    recipeDetails.addEventListener("click", function (event) {
+      var parentEL = event.target.parentElement;
+      console.log("DATA SET", parentEL);
+      var parentDataSet = parentEL.getAttribute("data-set");
+      console.log("IDD", parentDataSet);
+      localStorage.setItem("recipeId", parentDataSet);
+      localStorage.setItem("recipeName", recipeName);
+    });
+    recipeDetails.classList.add("card-text", "c-link");
+    recipeDetails.style.textDecoration = "none";
+    recipeDetails.style.color = "white";
+    recipeDetails.setAttribute("href", "instructions.html");
+    recipeDetails.setAttribute("target", "_blank");
+    var detailsText = document.createElement("small");
+    detailsText.textContent = "See instruction details";
+    recipeDetails.append(detailsText);
+
+    var ingredientsText = document.createElement("p");
+    ingredientsText.classList.add("card-text");
+    ingredientsText.textContent = "Ingredients : ";
+
+    for (var j = 0; j < ingredients.length; j++) {
+      //console.log("INDIVIDUAL MI", ingredients[j].name);
+      ingredientsText.textContent += ingredients[j].name;
+      if (j !== ingredients.length - 1) {
+        ingredientsText.textContent += ", ";
+      } else {
+        ingredientsText.textContent += ".";
+      }
+    }
+
+    bodyDIV.append(recipeTitle);
+    bodyDIV.append(ingredientsText);
+    bodyDIV.append(recipeDetails);
+    bodyContainer.append(bodyDIV);
+
+    rowDiv.append(imgDiv);
+    rowDiv.append(bodyContainer);
+    randomRecipeCard.append(rowDiv);
+    recipeContainer.append(randomRecipeCard);
+    mainRcontiner.append(recipeContainer);
+  }
+  console.log("RECIPE CONTAINER", recipeContainer);
+};
+
+requestRandomRecipe();
+
+formEL.addEventListener("submit", submitInfo);
+
+// var assignRecipeTitleID = function (id) {
+//   var recipeNameID;
+//   return (recipeNameID = id);
+// };
+document.addEventListener("DOMContentLoaded", () => {
+  (document.querySelectorAll(".notification .delete") || []).forEach(
+    ($delete) => {
+      const $notification = $delete.parentNode;
+
+      $delete.addEventListener("click", () => {
+        $notification.parentNode.removeChild($notification);
+        ingredientsLabelHelp.classList.add("text-dark");
+      });
+    }
+  );
+});
